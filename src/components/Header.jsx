449c8a1b31bf0712265bd421d4f1875292a7e6e3
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2024-11-25 14:34:30 -0600

    adjusting notify-badge animation time to 60s

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
           Just Notify
         </h1>
 
-        {/* <h1 className='text-5xl sm:text-8xl z-50 duration-75'>JUST NOTIFY</h1> */}
+        <a href="https://tailwindcss.com/" target='_blank' title='Tailwind documentation' className="group py-2  duration-300 text-white z-50 ">
 
-        <a href="https://tailwindcss.com/" target='_blank' title='Tailwind documentation' className="group py-2  duration-300 text-white z-50 hover:underline decoration-pink-500 underline-offset-2">
-          <h3 className="group-hover: flex gap-x-1 text-sm font-medium bg-clip-text text-transparent bg-gradient-to-r from-rose-500 to-violet-700 group-hover:from-rose-600 group-hover:to-violet-800 ">
+
+          <h3 className="group-hover: flex gap-x-1 text-xs font-medium bg-clip-text text-transparent bg-gradient-to-r from-rose-500 to-violet-700 group-hover:from-rose-600 group-hover:to-violet-800 ">
             Made with Tailwind
             <LinkIcon className="group-hover:scale-110 w-3.5 h-3.5 inline text-blue-500 duration-150"/>
           </h3>
@@ -21,7 +21,6 @@ const Header = () => {
 
       </div>
 
-      {/* <div className="w-full h-full blur-xl bg-gradient-to-t from-[#242424] from-10%  to-indigo-700  absolute top-0  opacity-500" /> */}
     </div>
   )
 }
