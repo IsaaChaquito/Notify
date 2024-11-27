a5b34eaf0103a4c20f978cb55f1f7787a07e28e2
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2024-11-26 21:07:57 -0600

    footer: github and linkedin icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,37 @@
+import { GithubIcon } from '../assets/icons/github'
 import { HeartIcon } from '../assets/icons/heart'
+import { LinkedInIcon } from '../assets/icons/linkedin'
 
 export const Footer = () => {
   return (
-    <div className=" p-5 w-full h-32 flex flex-col justify-around items-center  gap-5 bg-black border-t border-slate-600">
+    <div className=" py-5 w-full h-auto flex flex-col justify-around items-center  gap-5 bg-black border-t border-slate-600">
       <p className="flex justify-center items-center text-slate-400 rounded-full ">
         Created with 
         <HeartIcon title='Love' className="w-5 h-5 mx-1"/> by 
           <a 
             title='Isaac Castro G. on LinkedIn'
-            className="pl-1 font-['mae\_culpa'] text-xl text-blue-500 underline underline-offset-2 decoration-1" 
+            className="pl-1 font-['mae\_culpa'] text-xl text-blue-500 hover:text-blue-600 underline underline-offset-2 decoration-1" 
             href="https://www.linkedin.com/in/isaac-castro-guzmán-603a34211" target="_blank"
           >
             Isaac Castro G.
           </a>
       </p>
 
-      <span className="text-xs">
+      <div className='icon-section flex justify-center items-center gap-x-3 *:text-gray-500 '>
+        <a href="https://github.com/IsaaChaquito/Notify" target='_blank' className="cursor-pointer ">
+          <GithubIcon className="w-5 h-5 hover:text-gray-400 duration-300"/>
+        </a>
+
+        <a href="https://www.linkedin.com/in/isaac-castro-guzmán-603a34211" target='_blank' className="cursor-pointer ">
+          <LinkedInIcon className="w-5 h-5 cursor-pointer hover:text-gray-400 duration-300"/>
+        </a>
+        
+      </div>
+
+      <span className="text-xs text-slate-400">
         <span className="">{ `© ${new Date().getFullYear()} `}</span>
         <a 
-          href="https://github.com/IsaaChaquito" target="_blank" className="cursor-pointer underline text-blue-500 ">
+          href="https://github.com/IsaaChaquito" target="_blank" className="cursor-pointer underline text-blue-500 hover:text-blue-600">
             IsaaChaquito 
         </a>
         <span className="">{ `, Inc. All rights reserved`} </span>
